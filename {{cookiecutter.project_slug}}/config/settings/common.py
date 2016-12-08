6c1cf6e4dd772c73586f91d1b99f4a737b0619a2
Eddy <eddy.hung@gmail.com>
2016-12-08 16:56:49 +0100

    Update common.py

--- {{cookiecutter.project_slug}}/config/settings/common.py
+++ {{cookiecutter.project_slug}}/config/settings/common.py
@@ -44,7 +44,7 @@ THIRD_PARTY_APPS = (
 
 # Apps specific for this project go here.
 LOCAL_APPS = (
-    '{{ cookiecutter.project_slug }}.commmon',  # shared librairies
+    '{{ cookiecutter.project_slug }}.common',  # shared librairies
     '{{ cookiecutter.project_slug }}.users.apps.UsersConfig',  # custom users app
     # Your stuff: custom apps go here
 )
