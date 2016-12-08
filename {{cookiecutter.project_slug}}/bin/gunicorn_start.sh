c6c50248487afea104cac511f7383a65fbd72159
Eddy Hung <eddy.hung@gmail.com>
2016-12-08 08:23:50 +0100

    update

---
+++ {{cookiecutter.project_slug}}/bin/gunicorn_start.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+NAME="{{cookiecutter.project_slug}}"                                  # Name of the application
+DJANGODIR=/home/django/{{cookiecutter.project_slug}}             # Django project directory
+SOCKFILE=/home/django/{{cookiecutter.project_slug}}/run/gunicorn.sock  # we will communicte using this unix socket
+USER=<fill_user>                                        # the user to run as
+GROUP=<fill_group>                                     # the group to run as
+NUM_WORKERS=3                                     # how many worker processes should Gunicorn spawn
+DJANGO_SETTINGS_MODULE=config.settings.production             # which settings file should Django use
+DJANGO_WSGI_MODULE=config.wsgi                     # WSGI module name
+
+echo "Starting $NAME as `whoami`"
+
+# Activate the virtual environment
+cd $DJANGODIR
+source ./env_{{cookiecutter.project_slug}}/bin/activate
+export DJANGO_SETTINGS_MODULE=$DJANGO_SETTINGS_MODULE
+export PYTHONPATH=$DJANGODIR:$PYTHONPATH
+
+# Create the run directory if it doesn't exist
+RUNDIR=$(dirname $SOCKFILE)
+test -d $RUNDIR || mkdir -p $RUNDIR
+
+# Start your Django Unicorn
+# Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
+exec ./env_{{cookiecutter.project_slug}}/bin/gunicorn ${DJANGO_WSGI_MODULE}:application \
+  --name $NAME \
+  --workers $NUM_WORKERS \
+  --user=$USER --group=$GROUP \
+  --bind=unix:$SOCKFILE \
+  --log-level=debug \
+  --log-file=-
