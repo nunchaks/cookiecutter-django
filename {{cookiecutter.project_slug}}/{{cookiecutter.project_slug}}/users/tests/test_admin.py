dc59c80b30df569211b378817ab2da0ed727343d
Eddy Hung <eddy.hung@gmail.com>
2016-12-24 06:07:17 +0100

    update test case

--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/test_admin.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/test_admin.py
@@ -4,7 +4,6 @@ from ..admin import MyUserCreationForm
 
 
 class TestMyUserCreationForm(TestCase):
-
     def setUp(self):
         self.user = self.make_user()
 
@@ -12,8 +11,8 @@ class TestMyUserCreationForm(TestCase):
         # Instantiate the form with a new username
         form = MyUserCreationForm({
             'username': 'alamode',
-            'password1': '123456',
-            'password2': '123456',
+            'password1': 'PuhQpXBXGYs4',
+            'password2': 'PuhQpXBXGYs4',
         })
         # Run is_valid() to trigger the validation
         valid = form.is_valid()
@@ -27,8 +26,8 @@ class TestMyUserCreationForm(TestCase):
         # Instantiate the form with the same username as self.user
         form = MyUserCreationForm({
             'username': self.user.username,
-            'password1': '123456',
-            'password2': '123456',
+            'password1': 'PuhQpXBXGYs4',
+            'password2': 'PuhQpXBXGYs4',
         })
         # Run is_valid() to trigger the validation, which is going to fail
         # because the username is already taken
@@ -38,3 +37,58 @@ class TestMyUserCreationForm(TestCase):
         # The form.errors dict should contain a single error called 'username'
         self.assertTrue(len(form.errors) == 1)
         self.assertTrue('username' in form.errors)
+
+    def test_choose_strong_password(self):
+        # Instantiate user with password similar with username
+        form = MyUserCreationForm({
+            'username': 'alamode',
+            'password1': 'alamode1',
+            'password2': 'alamode1',
+        })
+        valid = form.is_valid()
+        self.assertFalse(valid)
+
+        # Instantiate user with short length password
+        form = MyUserCreationForm({
+            'username': 'alamode',
+            'password1': 'aZ3',
+            'password2': 'aZ3',
+        })
+        valid = form.is_valid()
+        self.assertFalse(valid)
+
+        # Instantiate user with common password
+        form = MyUserCreationForm({
+            'username': 'alamode',
+            'password1': 'azerty',
+            'password2': 'azerty',
+        })
+        valid = form.is_valid()
+        self.assertFalse(valid)
+
+        # Instantiate user with common password
+        form = MyUserCreationForm({
+            'username': 'alamode',
+            'password1': 'password',
+            'password2': 'password',
+        })
+        valid = form.is_valid()
+        self.assertFalse(valid)
+
+        # Instantiate user with only numeric password
+        form = MyUserCreationForm({
+            'username': 'alamode',
+            'password1': '154678',
+            'password2': '154678',
+        })
+        valid = form.is_valid()
+        self.assertFalse(valid)
+
+        # Instantiate user with strong password
+        form = MyUserCreationForm({
+            'username': 'alamode',
+            'password1': 'PuhQpXBXGYs4',
+            'password2': 'PuhQpXBXGYs4',
+        })
+        valid = form.is_valid()
+        self.assertTrue(valid)
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/test_models.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/users/tests/test_models.py
@@ -2,9 +2,10 @@ from test_plus.test import TestCase
 
 
 class TestUser(TestCase):
-
     def setUp(self):
         self.user = self.make_user()
+        self.user.first_name = 'John'
+        self.user.last_name = 'Doe'
 
     def test__str__(self):
         self.assertEqual(
@@ -17,3 +18,10 @@ class TestUser(TestCase):
             self.user.get_absolute_url(),
             '/users/testuser/'
         )
+
+    def test_short_name(self):
+        self.assertEqual(self.user.short_name, 'John D.')
+        self.user.last_name = ''
+        self.assertEqual(self.user.short_name, 'John')
+        self.user.first_name = ''
+        self.assertEqual(self.user.short_name, 'testuser')
