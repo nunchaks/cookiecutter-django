cefe0cce5840c9a68292897b3360fbdc444477c6
Eddy Hung <eddy.hung@gmail.com>
2017-03-22 07:52:01 +0100

    update settings

--- {{cookiecutter.project_slug}}/config/settings/production.py
+++ {{cookiecutter.project_slug}}/config/settings/production.py
@@ -67,18 +67,54 @@ MIDDLEWARE = (
 # See https://docs.djangoproject.com/en/1.9/ref/middleware/#module-django.middleware.security
 # and https://docs.djangoproject.com/ja/1.9/howto/deployment/checklist/#run-manage-py-check-deploy
 
-# set this to 60 seconds and then to 518400 when you can prove it works
+# HTTP Strict Transport Security - Force modern browsers to connect to the domain name
+# only through HTTPS and refuse insecure connection through this time.
+# Set this to 60 seconds and then to 518400 (6 days) when you can prove it works
+# Should be used only if everything is sure to be served by HTTPS and stay this way!
 SECURE_HSTS_SECONDS = 60
+
+# Add includeSubDomains tag to subdomains. Apply if all subdomains are sure to be in HTTPS as well...
 SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool(
     'DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS', default=True)
+
+# Only helps if Django is the front server for serving MEDIA files
+# In this case, activate this setting if you wish for the browser not to guess the Content-Type automatically
+# and only refer to the Content-Type header, to avoid potential security issue.
 SECURE_CONTENT_TYPE_NOSNIFF = env.bool(
     'DJANGO_SECURE_CONTENT_TYPE_NOSNIFF', default=True)
+
+# Force browser to block what seems to be XSS attack (JS in GET or POST parameters for instance)
 SECURE_BROWSER_XSS_FILTER = True
+
+# Whether to use a secure cookie for the session cookie.
+# If this is set to True, the cookie will be marked as “secure,” which means browsers may ensure
+# that the cookie is only sent under an HTTPS connection.
 SESSION_COOKIE_SECURE = True
+
+# Whether to use HTTPOnly flag on the session cookie. If this is set to True,
+# client-side JavaScript will not to be able to access the session cookie.
+# There’s not much excuse for leaving this off, either: if your code depends on
+# reading session cookies from JavaScript, you’re probably doing it wrong.
 SESSION_COOKIE_HTTPONLY = True
+
+# Redirect HTTP to HTTPS through 301. Should be done through nginx for instance for performance reason.
+# Intended to be used when this is not an option.
 SECURE_SSL_REDIRECT = env.bool('DJANGO_SECURE_SSL_REDIRECT', default=True)
+
+# Whether to use a secure cookie for the CSRF cookie.
 CSRF_COOKIE_SECURE = True
+
+# Whether to use HttpOnly flag on the CSRF cookie.
+# If you enable this and need to send the value of the CSRF token with Ajax requests, your JavaScript will need
+# to pull the value from a hidden CSRF token form input on the page instead of from the cookie.
 CSRF_COOKIE_HTTPONLY = True
+
+# Used to prevent clickjacking - fraudulous use of iframe to make people click on other sites without their consent.
+# Modern browsers honor the X-Frame-Options HTTP header that indicates whether or not
+# a resource is allowed to load within a frame or iframe.
+# If the response contains the header with a value of SAMEORIGIN then the browser will only load the resource
+# in a frame if the request originated from the same site. If the header is set to DENY then the browser will
+# block the resource from loading in a frame no matter which site made the request.
 X_FRAME_OPTIONS = 'DENY'
 
 # SITE CONFIGURATION
@@ -88,7 +124,7 @@ X_FRAME_OPTIONS = 'DENY'
 ALLOWED_HOSTS = env.list('DJANGO_ALLOWED_HOSTS', default=['{{cookiecutter.domain_name}}'])
 # END SITE CONFIGURATION
 
-INSTALLED_APPS += ('gunicorn', 'django_jenkins',)
+INSTALLED_APPS += ('gunicorn', 'django_jenkins', 'django_extensions')
 
 
 # STORAGE CONFIGURATION
@@ -151,7 +187,9 @@ INSTALLED_APPS = ('collectfast', ) + INSTALLED_APPS
 {% if cookiecutter.use_compressor == 'y'-%}
 # COMPRESSOR
 # ------------------------------------------------------------------------------
+{% if cookiecutter.use_aws_for_media -%}
 COMPRESS_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+{%- endif %}
 COMPRESS_URL = STATIC_URL
 COMPRESS_ENABLED = env.bool('COMPRESS_ENABLED', default=True)
 {%- endif %}
@@ -187,7 +225,8 @@ DATABASES['default'] = env.db('DATABASE_URL')
 # CACHING
 # ------------------------------------------------------------------------------
 
-REDIS_LOCATION = '{0}/{1}'.format(env('REDIS_URL', default='redis://127.0.0.1:6379'), 0)
+REDIS_LOCATION = '{0}/{1}'.format(env('REDIS_URL', default='redis://127.0.0.1:6379'),
+                                  env.int('REDIS_NUM_DB', default=0))
 # Heroku URL does not pass the DB number, so we parse it in
 CACHES = {
     'default': {
